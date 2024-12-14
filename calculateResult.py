2e7ed1b4265e1b08a5753ee2da6436f9e4021e21
Ken Keller <YoyodyneData@gmail.com>
2024-12-13 23:02:50 -0500

    Create calculateResult.py
    calculate the total win for each hand

---
+++ calculateResult.py
@@ -0,0 +1,19 @@
+calculateResult (bankroll, hands, dealerHand)
+  # loop through the playerHands in the array hands 
+  # add up the total of wins and subtract the total losses and return the amount 
+  totalWin = 0 
+  foreach playerHand in hands
+     if playerHand.result = '' 
+           if playerHand.total = dealerHand.total 
+			playerHand.result = 'P'
+           if playerHand.total > dealerHand.total 
+                        playerHand.result = 'W'
+           else 
+               playerHand.result = 'L'
+
+      if playerHand.result = 'L'
+           totalWin = totalWin - playerHand.betAmt 
+      if playerHand.result = 'W'
+           totalWin = totalWin + playerHand.betAmt
+
+return totalWin
