3ec9a666f344292b047b08b322d58812fea3619b
Ken Keller <YoyodyneData@gmail.com>
2024-12-13 21:10:17 -0500

    Update PlayerHand.py
    updating methods

--- PlayerHand.py
+++ PlayerHand.py
@@ -1,21 +1,24 @@
 # class for each hand that is played 
 # the value and suit of each card will be added each time a card is added to the PlayerHand 
 class playerHand:
-   def __init__(self, betAmt):
+   betAmt = 0
+   cards = []
+   
+   def __init__(self, betAmt, card):
      self.bet = betAmt
-     self.cards = []
+     self.cards.append(card)
 
    def take_a_hit (self, card):
      self.cards.append(card)
 
-   def handTotal():
+   def handTotal(self):
      for c in self.cards:
         total += c.value;
 
-   def doubleDown():
-      betAmt += betAmt
+   def increaseBet(self, bet)
+       self.betAmt = self.betAmt + bet 
 
-   def handValue():
+   def handValue(self):
       handValue = 0
       if (self.cards[0].value=10 && self.cards[1].value=1) handValue = 21
       if (self.cards[0].value=1 && self.cards[1].value=10) handValue = 21
