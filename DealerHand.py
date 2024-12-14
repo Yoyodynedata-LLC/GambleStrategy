d6bc073fc20ca7a6e193d0a49ccbec8436ba461d
Ken Keller <YoyodyneData@gmail.com>
2024-12-13 21:06:21 -0500

    Update DealerHand.py
    calculating hand steps

--- DealerHand.py
+++ DealerHand.py
@@ -2,20 +2,21 @@
 # the value and suit of each card will be added each time a card is added to the dealerHand
 # total is calculated as soft or hard total
 class dealerHand:
+   cards - array holding the cards 
+   
    def __init__(self, card):
      self.cards = [card]
 
    def take_a_hit (self, card):
      self.cards.append(card)
 
-   def handTotal():
-     for c in self.cards:
-        total += c.value;
-
-   def handValue():
-      handValue = 0
+   def handTotal(self):
       if (self.cards[0].value=10 && self.cards[1].value=1) handValue = 21
       if (self.cards[0].value=1 && self.cards[1].value=10) handValue = 21
+      
+      for c in self.cards:
+            total += c.value
+   
 
       
    
